2ce25b0a936a134dab9f34b7f31bd9105839806a
trangnn-cloudaccess <trangnn@cloudaccess.net>
2018-09-24 10:52:09 +0700

    Don't show activate success alert box, don't show progress bar when activate fail

--- joomla-sdk/source/packages/com_cloudaccessapi/site/views/activation/tmpl/default.php
+++ joomla-sdk/source/packages/com_cloudaccessapi/site/views/activation/tmpl/default.php
@@ -12,43 +12,45 @@ JHTML::_('behavior.modal');
 $support_mail = JComponentHelper::getParams('com_cloudaccessapi')->get('from');
 $creation_failed_message = JText::_('COM_CLOUDACCESSAPI_CREATION_FAILED') . " <a href=\"mailto:{$support_mail}\">{$support_mail}</a>";
 ?>
+
+<?php if (JFactory::getSession()->get('ca_activate_result')): ?>
 <div id="activationmodal" style="text-align: center;">
     <img src="<?php echo JFactory::getURI()->root(); ?>modules/mod_createcloudaccessdemo/assets/images/loading.gif" />
     <p><?php echo JText::_('COM_CLOUDACCESSAPI_CREATION_SITE'); ?></p>
 </div>
 <script>
-window.addEvent('domready', function() {
-    SqueezeBox.initialize();
-    SqueezeBox.open($('activationmodal'), {
-        handler: 'adopt',
-        size: {x: 300, y: 100},
-        closable: false,
-        closeBtn: false,
-        onOpen: function ()
-        {
-            jQuery('#sbox-btn-close').hide();
-            window.setTimeout('checkProcess()', 5000);
-        }
+    window.addEvent('domready', function() {
+        SqueezeBox.initialize();
+        SqueezeBox.open($('activationmodal'), {
+            handler: 'adopt',
+            size: {x: 300, y: 100},
+            closable: false,
+            closeBtn: false,
+            onOpen: function ()
+            {
+                jQuery('#sbox-btn-close').hide();
+                window.setTimeout('checkProcess()', 5000);
+            }
+        });
     });
-});
 
-function checkProcess()
-{
-    jQuery.ajax({
-        dataType: 'json',
-        url: 'index.php?option=com_cloudaccessapi&task=checkStatus&format=json',
-        cache: false,
-        success: function (response)
-        {
-            var emptyResponse = jQuery.isEmptyObject(response);
-            var found = false;
+    function checkProcess()
+    {
+        jQuery.ajax({
+            dataType: 'json',
+            url: 'index.php?option=com_cloudaccessapi&task=checkStatus&format=json',
+            cache: false,
+            success: function (response)
+            {
+                var emptyResponse = jQuery.isEmptyObject(response);
+                var found = false;
 
-            if (response == null || response.length === 0) {
-                jQuery('#activationmodal').text('<?php echo JText::_('COM_CLOUDACCESSAPI_CREATION_SUCCESS'); ?>');
-                window.setTimeout( 'SqueezeBox.close();', 5000 );
-                return false;
-            } else {
-                jQuery.each( response, function( pid, process ) {
+                if (response == null || response.length === 0) {
+                    jQuery('#activationmodal').text('<?php echo JText::_('COM_CLOUDACCESSAPI_CREATION_SUCCESS'); ?>');
+                    window.setTimeout( 'SqueezeBox.close();', 5000 );
+                    return false;
+                } else {
+                    jQuery.each( response, function( pid, process ) {
                         if (process.status != 'running' && process.status != 'pending') {
                             jQuery('#activationmodal').text('').html('');
                             var p = document.createElement('p');
@@ -64,20 +66,20 @@ function checkProcess()
                         } else {
                             found = true;
                         }
-                });
-            }
+                    });
+                }
 
-            if (found) {
-                window.setTimeout('checkProcess()', 5000);
-            } else {
+                if (found) {
+                    window.setTimeout('checkProcess()', 5000);
+                } else {
+                    window.setTimeout( 'SqueezeBox.close();document.location.href="";', 8000 );
+                }
+            },
+            error: function (jqXHR, textStatus, errorThrown){
+                jQuery('#activationmodal').text(textStatus);
                 window.setTimeout( 'SqueezeBox.close();document.location.href="";', 8000 );
             }
-        },
-        error: function (jqXHR, textStatus, errorThrown){
-            jQuery('#activationmodal').text(textStatus);
-            window.setTimeout( 'SqueezeBox.close();document.location.href="";', 8000 );
-        }
-    });
-}
-
+        });
+    }
 </script>
+<?php endif; ?>
